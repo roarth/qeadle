d8e9dcaaa1e74a575b52314d6cf2023c4f3676b8
Mirete Benoit <benoit.mirete@cybelangel.com>
2021-06-20 19:36:31 +0200

    Create Projects Module w/ Controller & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [],
+  imports: [ProjectsModule],
   controllers: [],
   providers: [],
 })
